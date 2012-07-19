83521db8d774616828cf80310d34219882b2dcfb
Dat Ngo <ngothiendat@gmail.com>
2012-07-19 16:04:06 +0700

    using oauth2 instead of oauth

--- lib/omniauth/strategies/afterclassroom.rb
+++ lib/omniauth/strategies/afterclassroom.rb
@@ -1,12 +1,17 @@
-require 'omniauth-oauth'
+require 'omniauth-oauth2'
 require 'multi_json'
 
 module OmniAuth
   module Strategies
-    class Afterclassroom < OmniAuth::Strategies::OAuth
+    class Afterclassroom < OmniAuth::Strategies::OAuth2
       option :name, 'afterclassroom'
-      option :client_options, {:authorize_path => '/oauth/afterclassroom',
-                               :site => 'http://localhost:3000'}#https://afterclassroom.com
+      option :client_options, {
+        :authorize_path => '/oauth/authorize',#'/oauth/afterclassroom',
+        :site => 'http://localhost:3000',
+        :request_token_path => "/oauth/token",
+      }#https://afterclassroom.com
+
+      
 
       uid { access_token.params[:user_id] }
 
@@ -21,8 +26,16 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/api/user').parsed
+        @raw_info ||= access_token.get('/api/users/me').parsed
+#        @raw_info ||= MultiJson.load(access_token.get('/api/users/me').parsed)
       end
+
+      alias :old_request_phase :request_phase
+
+
+
+
+
 
     end
   end
